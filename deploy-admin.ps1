4ae81f007722e447079ded135d21c18188281597
Nicolas Claverie <claverie.nicolas.lapo@gmail.com>
2024-11-16 23:07:25 +0100

    ajout script non fini

--- deploy-admin.ps1
+++ deploy-admin.ps1
@@ -1,47 +1,70 @@
-# Activer la protection système pour le disque C:
-Write-Output "Activation de la protection système sur le disque C:..."
+
+#######################################################################
+#                                                                     #
+#    SCRIPT POUR DEPLOIMENT POSTE DE TRAVAIL, A APPLIQUER EN ADMIN    #
+#                                                                     #
+#######################################################################
+
+
+#---------------------------------
+#
+# Lancement de l'installation Sentinel One
+#
+#---------------------------------
+
+
+# Copier le token dans le presse-papier
+Get-Content -Path "C:\Users\Testeur\Documents\toto.txt" | Set-Clipboard # A changer
+
+# Lancement de l'installation
+Start-Process -FilePath "C:\chemin\vers\programme.exe" # A changer
+
+
+#---------------------------------
+#
+# Activation de la protection système pour le disque C:
+#
+#---------------------------------
+
+
+Write-Host "Activation de la protection système sur le disque C:..."
+
+# Reglage limite max de l'utilisation espace disque
 vssadmin resize shadowstorage /For=C: /On=C: /MaxSize=5%
 
-# Vérifier si la protection est déjà activée
-$protectionStatus = Get-ComputerRestorePoint -Drive C: -ErrorAction SilentlyContinue
+# Activer la protection 
+Enable-ComputerRestore -Drive "C:\"
 
-if (-not $protectionStatus) {
-    Write-Output "Activation de la protection système..."
-    Enable-ComputerRestorePoint -Drive C:
-} else {
-    Write-Output "La protection système est déjà activée pour le disque C:."
-}
+# Ouverture de la fenetre protection systeme pour verification
+SystemPropertiesProtection
 
-# Définir l'utilisation maximale de l'espace disque pour les points de restauration
-Write-Output "Définition de l'utilisation maximale à 5%..."
-vssadmin resize shadowstorage /For=C: /On=C: /MaxSize=5%
-
-Write-Output "Protection système configurée avec succès pour le disque C:."
+Write-Host "Protection système configurée avec succès pour le disque C:."
 
 
-
-
+#---------------------------------
+#
+# Changement nom, description et domaine du pc
+#
+#---------------------------------
 
 
 # Demander le nouveau nom de l'ordinateur
+Write-host "Pensez a ajouter X au nom de l'ordinateur" -ForegroundColor Red
 $newComputerName = Read-Host "Entrez le nouveau nom de l'ordinateur"
 
 # Demander la nouvelle description de l'ordinateur
 $newDescription = Read-Host "Entrez la description de l'ordinateur"
 
-# Définir le domaine
-$domainName = "TER_SUD"
-
 # Changer le nom de l'ordinateur
-Write-Output "Changement du nom de l'ordinateur en '$newComputerName'..."
+Write-Output "Changement du nom de l'ordinateur en '$newComputerName' "
 Rename-Computer -NewName $newComputerName -Force
 
 # Changer la description de l'ordinateur dans le registre
-Write-Output "Modification de la description en '$newDescription'..."
+Write-Output "Modification de la description en '$newDescription' "
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "srvcomment" -Value $newDescription
 
-# Joindre l'ordinateur au domaine (si ce n'est pas déjà fait)
-Write-Output "Ajout de l'ordinateur au domaine '$domainName'..."
-Add-Computer -DomainName $domainName -Credential (Get-Credential) -Force -Restart
+# Joindre l'ordinateur au domaine
+Write-Output "Ajout de l'ordinateur au domaine 'TER_SUD' "
+Add-Computer -DomainName TER_SUD -Credential (Get-Credential) -Force -Restart
 
 Write-Output "Les modifications ont été appliquées. L'ordinateur va redémarrer."

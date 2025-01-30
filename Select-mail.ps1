06d69d9ceff8109641ab34072fceaaa01f04e8c4
NicoClaverie <claverie.nicolas.lapo@gmail.com>
2025-01-30 13:33:00 +0100

    ajout de colonne et informations pour la duplication d'information

--- Select-mail.ps1
+++ Select-mail.ps1
@@ -4,12 +4,23 @@ $ExtractLot =  Import-Csv "$env:USERPROFILE\documents\test\lot2.csv"
 $ExtractMail = Import-Csv "$env:userprofile\documents\test\glpi.csv"
 $CSVSortie = "$env:USERPROFILE\documents\test\Mail-Lot.csv"
 
+# Fonction pour extraire le prénom + nom (derniers mots de la colonne Utilisateur)
+function Extraire-Nom {
+    param ([string]$texte)
+    $mots = $texte -split " "
+    if ($mots.Count -ge 2) {
+        return "$($mots[-2]) $($mots[-1])"  # Derniers mots = Prénom + Nom
+    }
+    return $texte
+}
+
+
 
 # Comparer les fichiers
 
 $resultat = foreach ($ligne1 in $ExtractLot) {
-    $NomRechercher = $ligne1.UTILISATEUR 
-    $LigneCorrespondante = $ExtractMail | Where-Object { $_.Nom -eq $NomRechercher }
+    $NomRechercher = Extraire-Nom $ligne1.UTILISATEUR 
+    $LigneCorrespondante = $ExtractMail | Where-Object { $_.Nom -eq $NomRechercher -replace " ", "." }  # Vérifie si le nom apparaît dans l'e-mail
     if ($LigneCorrespondante) {
         $LigneCorrespondante | Select-Object "Adresses de messagerie"
     }

0d2a97d67af53ee14bdee0913edcd931c48b2997
NicoClaverie <claverie.nicolas.lapo@gmail.com>
2025-04-25 13:36:00 +0200

    modification du pilote Xerox et condition pour installer une imprimante en HP

--- Boulot/add-printer-IP-V1.2.ps1
+++ Boulot/add-printer-IP-V1.2.ps1
@@ -8,9 +8,9 @@
 
 do {
 
-# Permet une élévation de privilège
+# Permet une elevation de privilege
     if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        Write-Host "Redémarrage du script avec privilèges administratifs..." -ForegroundColor Yellow
+        Write-Host "Redemarrage du script avec privileges administratifs..." -ForegroundColor Yellow
         $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`""
         Start-Process -FilePath "powershell.exe" -ArgumentList $arguments -Verb RunAs
         Exit
@@ -32,10 +32,10 @@ do {
         Write-Host "4. Xerox"
         $choixPilote = Read-Host "Votre choix ?"
 
-        # Vérification si l'entrée est un nombre entier entre 1 et 4
+        # Verification si l'entree est un nombre entier entre 1 et 4
         if ($choixPilote -match '^\d+$' -and 1..4 -contains [int]$choixPilote) {
             $choixValide = $true
-            # Détermination du chemin du pilote en fonction du choix
+            # Determination du chemin du pilote en fonction du choix
             switch ($choixPilote) {
                 1 {
                     $cheminPilote = "C:\Imprimantes\CANON\GPlus_PCL6_Driver_V230_W64_00\Driver"
@@ -50,7 +50,7 @@ do {
                     $versionPilote = "Lexmark Universal v2 XL" 
                 }
                 4 {
-                    $cheminPilote = "C:\Imprimantes\XEROX\UNIV_5.703.12.0_PCL6_x64\UNIV_5.703.12.0_PCL6_x64_Driver.inf"
+                    $cheminPilote = "C:\Imprimantes\XEROX\UNIV_5.1035.2.0_PCL6_x64_Driver.inf"
                     $versionPilote = "Xerox Global Print Driver PCL6" 
                 }
             }
@@ -60,62 +60,81 @@ do {
         }
     }
 
-    # Affichage des résultats pour vérification
-    Write-Host "Chemin du pilote sélectionné : $cheminPilote"
-    Write-Host "Version du pilote sélectionnée : $versionPilote"
+    # Affichage des resultats pour verification
+    Write-Host "Chemin du pilote selectionne : $cheminPilote"
+    Write-Host "Version du pilote selectionnee : $versionPilote"
 
-    # Vérification de la présence du pilote
+    # Verification de la presence du pilote
     $piloteExistant = Get-PrinterDriver | Where-Object { $_.Name -eq $versionPilote }
 
     if ($piloteExistant) {
-        Write-Host "Le pilote '$versionPilote' est déjà installé." -ForegroundColor Green
+        Write-Host "Le pilote '$versionPilote' est dejà installe." -ForegroundColor Green
     }
     else {
-        Write-Host "Le pilote '$versionPilote' n'est pas installé. Installation en cours..." -ForegroundColor Yellow
+        Write-Host "Le pilote '$versionPilote' n'est pas installe. Installation en cours..." -ForegroundColor Yellow
         
         # Passe tout le repertoire en revu pour prendre tout les .inf en compte
         if (Test-Path $cheminPilote) {
         Get-ChildItem -Path $cheminPilote -Filter "*.inf" | ForEach-Object {
             pnputil /add-driver $_.FullName /install
         }
-            Write-Host "Pilote '$versionPilote' installé avec succès !" -ForegroundColor Green
+            Write-Host "Pilote '$versionPilote' installe avec succes !" -ForegroundColor Green
         }
         else {
-            Write-Host "Le fichier de pilote n'a pas été trouvé. Vérifiez le chemin." -ForegroundColor Red
+            Write-Host "Le fichier de pilote n'a pas ete trouve. Verifiez le chemin." -ForegroundColor Red
             Pause
             Exit
         }
     }
 
-    # Création du port et ajout de l'imprimante
-    Add-PrinterDriver -Name $versionPilote
-    Add-PrinterPort -Name $imprimante -PrinterHostAddress $adresseIP
-    Add-Printer -Name "$imprimante COULEUR" -PortName $imprimante -DriverName $versionPilote
-    Add-Printer -Name "$imprimante NB" -PortName $imprimante -DriverName $versionPilote
+    # Creation du port et ajout de l'imprimante
+    if ($choixPilote -eq 2) {
+        Add-PrinterDriver -Name $versionPilote
+        Add-PrinterPort -Name $imprimante -PrinterHostAddress $adresseIP
+        Add-Printer -Name "$imprimante" -PortName $imprimante -DriverName $versionPilote
 
-    # Vérification que les imprimantes ont bien été ajoutées
+
+        # Verification que les imprimantes ont bien ete ajoutees
+    $imprimanteAjoutee = Get-Printer | Where-Object { $_.Name -eq "$imprimante" }
+    
+    if ($imprimanteAjoutee) {
+        Write-Host "L'imprimante '$imprimante a ete ajoutees avec succes !" -ForegroundColor Green
+    }
+    else {
+            Write-Host "Erreur : L'imprimante '$imprimante' n'a pas ete trouvee." -ForegroundColor Red
+        }
+    }
+    
+    else {
+        Add-PrinterDriver -Name $versionPilote
+        Add-PrinterPort -Name $imprimante -PrinterHostAddress $adresseIP
+        Add-Printer -Name "$imprimante COULEUR" -PortName $imprimante -DriverName $versionPilote
+        Add-Printer -Name "$imprimante NB" -PortName $imprimante -DriverName $versionPilote
+    
+    # Verification que les imprimantes ont bien ete ajoutees
     $imprimanteCOULEURAjoutee = Get-Printer | Where-Object { $_.Name -eq "$imprimante COULEUR" }
     $imprimanteNBAjoutee = Get-Printer | Where-Object { $_.Name -eq "$imprimante NB" }
 
     if ($imprimanteCOULEURAjoutee -and $imprimanteNBAjoutee) {
-        Write-Host "Les imprimantes '$imprimante COULEUR' et '$imprimante NB' ont été ajoutées avec succès !" -ForegroundColor Green
+        Write-Host "Les imprimantes '$imprimante COULEUR' et '$imprimante NB' ont ete ajoutees avec succes !" -ForegroundColor Green
     }
     else {
-        Write-Host "Une ou plusieurs imprimantes n'ont pas été ajoutées correctement." -ForegroundColor Red
+        Write-Host "Une ou plusieurs imprimantes n'ont pas ete ajoutees correctement." -ForegroundColor Red
         if (-not $imprimanteCOULEURAjoutee) {
-            Write-Host "Erreur : L'imprimante '$imprimante COULEUR' n'a pas été trouvée." -ForegroundColor Red
+            Write-Host "Erreur : L'imprimante '$imprimante COULEUR' n'a pas ete trouvee." -ForegroundColor Red
         }
         if (-not $imprimanteNBAjoutee) {
-            Write-Host "Erreur : L'imprimante '$imprimante NB' n'a pas été trouvée." -ForegroundColor Red
+            Write-Host "Erreur : L'imprimante '$imprimante NB' n'a pas ete trouvee." -ForegroundColor Red
         }
     }
+}
 
 
     # Demande à l'utilisateur s'il souhaite ajouter une autre imprimante
     $reponse = Read-Host "Souhaitez-vous ajouter une autre imprimante ? (O/N)"
 } while ($reponse -match '^(O|o|Oui|oui)$')
 
-Write-Host "Script terminé. Bonne journée !" -ForegroundColor Green
+Write-Host "Script termine. Bonne journee !" -ForegroundColor Green
 
 
 

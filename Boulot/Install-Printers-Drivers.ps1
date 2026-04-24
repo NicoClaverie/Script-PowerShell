bea3da4a4728bfc3934adbb56b70dad50bafcde5
NicoClaverie <claverie.nicolas.lapo@gmail.com>
2026-04-24 15:25:16 +0200

    ajout de pilotes pour deploiement

--- Boulot/Install-Printers-Drivers.ps1
+++ Boulot/Install-Printers-Drivers.ps1
@@ -15,11 +15,17 @@ $driversConfig = @(
     @{ Path = "C:\Imprimantes\LEXMARK\Lexmark_Universal_v2_XL_3_0_2\Drivers\Print\GDI"; Name = "Lexmark Universal v2 XL" },
     # Pilotes pour imprimantes XEROX
     @{ Path = "C:\Imprimantes\XEROX\UNIV_5.1035.2.0_PCL6_x64_Driver.inf"; Name = "Xerox Global Print Driver PCL6" },
+    @{ Path = "C:\Imprimantes\XEROX\UNIV_5.496.7.0_PCL6_x64_Driver.inf"; Name = "Xerox Global Print Driver PCL6" },
+    @{ Path = "C:\Imprimantes\XEROX\UNIV_5.548.8.0_PCL_x64"; Name = "Xerox Global Print Driver PCL" },
+    @{ Path = "C:\Imprimantes\XEROX\UNIV_5.548.8.0_PCL6_x64"; Name = "Xerox Global Print Driver PCL6" },
+    @{ Path = "C:\Imprimantes\XEROX\UNIV_5.548.8.0_PS_x64"; Name = "Xerox Global Print Driver PS" },
+    @{ Path = "C:\Imprimantes\XEROX\UNIV_5.645.5.0_PCL6_x64"; Name = "Xerox Global Print Driver PCL6" },    
     # Pilotes pour imprimantes HP
     @{ Path = "C:\Imprimantes\HP\pcl6-x64-6.9.0.24630"; Name = "HP Universal Printing PCL 6" },
     @{ Path = "C:\Imprimantes\HP\pcl6-x64-6.6.0.23029"; Name = "HP Universal Printing PCL 6" },
     @{ Path = "C:\Imprimantes\HP\pcl6-x64-7.0.1.24923"; Name = "HP Universal Printing PCL 6" },
-    @{ Path = "C:\Imprimantes\HP\pcl6-x64-6.0.0.18849"; Name = "HP Universal Printing PCL 6" }
+    @{ Path = "C:\Imprimantes\HP\pcl6-x64-6.0.0.18849"; Name = "HP Universal Printing PCL 6" },
+    @{ Path = "C:\Imprimantes\HP\pcl6-x64-7.9.0.26347"; Name = "HP Universal Printing PCL 6" }
 )
 
 Write-Host "--- DEBUT DE L'INSTALLATION (AVEC RETRY) ---" -ForegroundColor Cyan
